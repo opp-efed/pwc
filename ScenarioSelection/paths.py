58eed8462bf9c585adaac63807d9aa83b30b7b94
triphook <asdflkj3>
2019-06-26 16:16:39 -0400

    moved some scripts to dev, added in tie handling

--- ScenarioSelection/paths.py
+++ ScenarioSelection/paths.py
@@ -4,7 +4,7 @@ input_dir = "Input"
 output_dir = "Output"
 
 pwc_batch_file = os.path.join(input_dir, "Samples", "corn_60k.csv")  # the table used as input for the pwc run
-pwc_summary_file = os.path.join("Input", "Samples", "Summary_SW_60k.txt")  # the output file from the pwc run
+pwc_summary_file = os.path.join(input_dir, "Samples", "Summary_SW_60k.txt")  # the output file from the pwc run
 output_dir = "Output"  # where output data will be written
 summary_outfile = os.path.join(output_dir, "test_summary.csv")
 selected_scenario_outfile = os.path.join(output_dir, "test_selection.csv")
